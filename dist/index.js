3e88b7159447caf62e3a252456b99f7e52ef7442
Francisco Javier Llantada <franciscollantada@gmail.com>
2022-09-26 11:24:57 -0300

    Delete dist directory

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ejercicio1_1 = require("./src/entrega1/ejercicio1");
-console.log("Inicio programas de ejercios");
-// Ejercicio 1
-// Crear una clase llamada Usuario que reciba un nombre y un apellido en el constructor y tenga un método getFullName que retorne el nombre y apellido unidos por un espacio...
-(0, ejercicio1_1.ejercicio1)();
